184d0a22fbb1aff91ec1290d0a74787c46f0c169
Daniel Lasota <slasher.root@gmail.com>
2024-09-08 17:27:22 +0200

    feat: Add calculate_indicators and plot_fetched_data methods

--- stonks/scraper.py
+++ stonks/scraper.py
@@ -1,4 +1,6 @@
+import pandas
 import yfinance as yf
+import matplotlib.pyplot as plt
 
 
 def run_stocks_analysis(asset: str) -> None:
@@ -7,4 +9,30 @@ def run_stocks_analysis(asset: str) -> None:
     if data.empty:
         print('No data fetched. Check the ticker symbol')
     else:
-        print(data)
+        data = calculate_indicators(df=data)
+        plot_fetched_data(df=data)
+
+def calculate_indicators(df: pandas.DataFrame) -> pandas.DataFrame:
+    df['SMA'] = df['Close'].rolling(window=20).mean()
+
+    df['STD'] = df['Close'].rolling(window=20).std()
+
+    df['Upper_Bollinger_Band'] = df['SMA'] + (df['STD'] * 2)
+    df['Lower_Bollinger_Band'] = df['SMA'] - (df['STD'] * 2)
+
+    return df
+
+def plot_fetched_data(df: pandas.DataFrame) -> None:
+    plt.figure(figsize=(14, 7))
+    plt.plot(df['Close'], label='Close Price', color='black')
+    plt.plot(df['SMA'], label='SMA', color='green', linewidth=0.8)
+    plt.plot(df['Upper_Bollinger_Band'], label='Upper Bollinger Band', color='red', linewidth=0.8)
+    plt.plot(df['Lower_Bollinger_Band'], label='Lower Bollinger Band', color='cyan', linewidth=0.8)
+
+    plt.title(f'Some Asset')
+    plt.xlabel('Date')
+    plt.ylabel('Close Price')
+    plt.legend(loc='upper left')
+    plt.grid(True)
+
+    plt.show()
