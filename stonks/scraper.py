08a05623163188d27e9567ece92381b13bf8559c
Daniel Lasota <slasher.root@gmail.com>
2024-09-09 23:45:46 +0200

    test: Tests add

--- stonks/scraper.py
+++ stonks/scraper.py
@@ -8,22 +8,18 @@ def run_stocks_analysis(asset: str) -> None:
     df = download_historical_data(asset=asset)
 
     if df.empty:
-        print('No data fetched. Check the ticker symbol')
-    else:
-        print(df)
-        print(df.columns)
-        df = calculate_indicators(df=df)
-        plot_fetched_data(df=df)
+        raise ValueError(f"No data fetched for ticker symbol '{asset}'. Check the ticker symbol.")
+
+    df = calculate_indicators(df=df)
+    plot_fetched_data(df=df)
 
 def download_historical_data(asset: str) -> pandas.DataFrame:
-    data = yf.download(asset, period='1mo', interval='1h')
-
-    return data
+    return yf.download(asset, period='1mo', interval='1h')
 
 def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
     df['SMA'] = df['Close'].rolling(window=20).mean()
 
-    df['STD'] = df['Close'].rolling(window=20).std()
+    df['STD'] = df['Close'].rolling(window=20).std().round(3)
     df['Upper_Bollinger_Band'] = df['SMA'] + (df['STD'] * 2)
     df['Lower_Bollinger_Band'] = df['SMA'] - (df['STD'] * 2)
 
---
+++ stonks/tests/test_scraper.py
@@ -0,0 +1,79 @@
+from io import StringIO
+import pytest
+import pandas as pd
+from stonks.scraper import download_historical_data, run_stocks_analysis, calculate_indicators
+from unittest.mock import patch
+
+
+class TestScraper:
+
+    def test_given_empty_dataframe_is_returned_when_passing_dataframe_then_exception_is_being_thrown(self):
+        # given
+        # when
+        with patch('stonks.scraper.download_historical_data', return_value=pd.DataFrame()):
+            # then
+            with pytest.raises(ValueError):
+                run_stocks_analysis('INVALID_ASSET')
+
+    def test_given_incorrect_asset_name_when_downloading_historical_data_then_empty_dataframe_is_returned(self):
+        # given
+        asset = 'NOT_STONKS'
+        # when
+        df = download_historical_data(asset)
+        # then
+        assert df.empty, "Expected empty dataframe for invalid asset"
+
+    def test_given_correct_asset_name_when_downloading_historical_data_then_non_empty_dataframe_is_returned(self):
+        # given
+        asset = 'EURUSD=X'
+        # when
+        df = download_historical_data(asset)
+
+        #then
+        assert not df.empty, "Expected non-empty dataframe for valid asset"
+        expected_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
+        assert all(column in df.columns for column in expected_columns), "Expected columns missing from DataFrame"
+
+    def test_given_ohlc_historical_dataframe_when_calculating_indicators_then_is_dataframe_with_correct_data_returned(self):
+        # given
+        data = {
+            'Close': [10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 29, 28, 27, 26]
+        }
+        df = pd.DataFrame(data)
+
+        # when
+        df_with_indicators = calculate_indicators(df.copy())
+
+        # then
+        expected_csv = StringIO(
+            """Close,SMA,STD,Upper_Bollinger_Band,Lower_Bollinger_Band
+            10,,,
+            11,,,
+            12,,,
+            13,,,
+            14,,,
+            15,,,
+            16,,,
+            17,,,
+            18,,,
+            19,,,
+            20,,,
+            21,,,
+            22,,,
+            23,,,
+            24,,,
+            25,,,
+            26,,,
+            27,,,
+            28,,,
+            29,19.5,5.916,31.332,7.668
+            30,20.5,5.916,32.332,8.668
+            29,21.4,5.762,32.924,9.876
+            28,22.2,5.493,33.186,11.214
+            27,22.9,5.139,33.178,12.622
+            26,23.5,4.730,32.960,14.040
+            """
+        )
+        expected_df = pd.read_csv(expected_csv)
+
+        pd.testing.assert_frame_equal(df_with_indicators.reset_index(drop=True), expected_df.reset_index(drop=True))
