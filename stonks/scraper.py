172dfdc728331dec95fe33438070d2986141ea29
Daniel Lasota <slasher.root@gmail.com>
2024-09-11 19:16:33 +0200

    refactor: encapsulation

--- stonks/scraper.py
+++ stonks/scraper.py
@@ -1,43 +1,51 @@
-import pandas
 import pandas as pd
 import yfinance as yf
 import mplfinance as mpf
 
 
+class Stonks:
+    def __init__(self, asset: str):
+        self.asset = asset
+
+        self.df = self.download_historical_data(asset)
+
+        if self.df.empty:
+            raise ValueError(f"No data fetched for ticker symbol '{self.asset}'. Check the ticker symbol.")
+
+        self.df = self.calculate_indicators(self.df)
+
+    @staticmethod
+    def download_historical_data(asset: str) -> pd.DataFrame:
+        return yf.download(asset, period='1mo', interval='1h')
+
+    @staticmethod
+    def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
+        df = df.copy()
+        df['SMA'] = df['Close'].rolling(window=20).mean()
+        df['STD'] = df['Close'].rolling(window=20).std().round(3)
+        df['Upper_Bollinger_Band'] = df['SMA'] + (df['STD'] * 2)
+        df['Lower_Bollinger_Band'] = df['SMA'] - (df['STD'] * 2)
+
+        return df
+
+    def plot_fetched_data(self) -> None:
+        df = self.df.copy()
+        df.index.name = 'Date'
+
+        indicators_width = 0.2
+
+        add_plots = [
+            mpf.make_addplot(df['SMA'], color='green', label='SMA', width=indicators_width),
+            mpf.make_addplot(df['Upper_Bollinger_Band'], color='red', linestyle='--', label='Upper Bollinger',
+                             width=indicators_width),
+            mpf.make_addplot(df['Lower_Bollinger_Band'], color='blue', linestyle='--', label='Lower Bollinger',
+                             width=indicators_width)
+        ]
+
+        mpf.plot(df, type='candle', style='charles', addplot=add_plots, title=f'{self.asset} Px Chart', ylabel='Price',
+                 volume=False, figsize=(14, 7), show_nontrading=True)
+
+
 def run_stocks_analysis(asset: str) -> None:
-    df = download_historical_data(asset=asset)
-
-    if df.empty:
-        raise ValueError(f"No data fetched for ticker symbol '{asset}'. Check the ticker symbol.")
-
-    df = calculate_indicators(df=df)
-    plot_fetched_data(df=df)
-
-def download_historical_data(asset: str) -> pandas.DataFrame:
-    return yf.download(asset, period='1mo', interval='1h')
-
-def calculate_indicators(df: pd.DataFrame) -> pd.DataFrame:
-    df['SMA'] = df['Close'].rolling(window=20).mean()
-
-    df['STD'] = df['Close'].rolling(window=20).std().round(3)
-    df['Upper_Bollinger_Band'] = df['SMA'] + (df['STD'] * 2)
-    df['Lower_Bollinger_Band'] = df['SMA'] - (df['STD'] * 2)
-
-    return df
-
-def plot_fetched_data(df: pd.DataFrame) -> None:
-    df.index.name = 'Date'
-
-    indicators_width = 0.2
-
-    add_plots = [
-        mpf.make_addplot(df['SMA'], color='green', label='SMA', width=indicators_width),
-        mpf.make_addplot(df['Upper_Bollinger_Band'], color='red', linestyle='--', label='Upper Bollinger',
-                         width=indicators_width),
-        mpf.make_addplot(df['Lower_Bollinger_Band'], color='blue', linestyle='--', label='Lower Bollinger',
-                         width=indicators_width)
-    ]
-
-    mpf.plot(df, type='candle', style='charles', addplot=add_plots,
-             title=f'px', ylabel='Price',
-             volume=False, figsize=(14, 7), show_nontrading=True)
+    stock_analysis = Stonks(asset)
+    stock_analysis.plot_fetched_data()
--- stonks/tests/test_scraper.py
+++ stonks/tests/test_scraper.py
@@ -1,25 +1,27 @@
 from io import StringIO
 import pytest
 import pandas as pd
-from stonks.scraper import download_historical_data, run_stocks_analysis, calculate_indicators
 from unittest.mock import patch
+
+from stonks.scraper import Stonks
 
 
 class TestScraper:
 
     def test_given_empty_dataframe_is_returned_when_passing_dataframe_then_exception_is_being_thrown(self):
         # given
+        asset = 'INVALID_ASSET'
         # when
-        with patch('stonks.scraper.download_historical_data', return_value=pd.DataFrame()):
+        with patch('stonks.scraper.Stonks.download_historical_data', return_value=pd.DataFrame()):
             # then
             with pytest.raises(ValueError):
-                run_stocks_analysis('INVALID_ASSET')
+                Stonks(asset)
 
     def test_given_incorrect_asset_name_when_downloading_historical_data_then_empty_dataframe_is_returned(self):
         # given
         asset = 'NOT_STONKS'
         # when
-        df = download_historical_data(asset)
+        df = Stonks.download_historical_data(asset)
         # then
         assert df.empty, "Expected empty dataframe for invalid asset"
 
@@ -27,10 +29,10 @@ class TestScraper:
         # given
         asset = 'EURUSD=X'
         # when
-        df = download_historical_data(asset)
+        df = Stonks.download_historical_data(asset)
 
         #then
-        assert not df.empty, "Expected non-empty dataframe for valid asset"
+        assert not df.empty, "Expected non empty dataframe for valid asset"
         expected_columns = ['Open', 'High', 'Low', 'Close', 'Volume']
         assert all(column in df.columns for column in expected_columns), "Expected columns missing from DataFrame"
 
@@ -42,30 +44,30 @@ class TestScraper:
         df = pd.DataFrame(data)
 
         # when
-        df_with_indicators = calculate_indicators(df.copy())
+        df_with_indicators = Stonks.calculate_indicators(df.copy())
 
         # then
         expected_csv = StringIO(
             """Close,SMA,STD,Upper_Bollinger_Band,Lower_Bollinger_Band
-            10,,,
-            11,,,
-            12,,,
-            13,,,
-            14,,,
-            15,,,
-            16,,,
-            17,,,
-            18,,,
-            19,,,
-            20,,,
-            21,,,
-            22,,,
-            23,,,
-            24,,,
-            25,,,
-            26,,,
-            27,,,
-            28,,,
+            10,,,,
+            11,,,,
+            12,,,,
+            13,,,,
+            14,,,,
+            15,,,,
+            16,,,,
+            17,,,,
+            18,,,,
+            19,,,,
+            20,,,,
+            21,,,,
+            22,,,,
+            23,,,,
+            24,,,,
+            25,,,,
+            26,,,,
+            27,,,,
+            28,,,,
             29,19.5,5.916,31.332,7.668
             30,20.5,5.916,32.332,8.668
             29,21.4,5.762,32.924,9.876
