a2fcd07aba18729c1ace5354ea4f5e1d2bcdcdaa
Daniel Lasota <slasher.root@gmail.com>
2024-09-11 19:31:46 +0200

    feat: Add selection panel to the gui

--- stonks/scraper.py
+++ stonks/scraper.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import yfinance as yf
 import mplfinance as mpf
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox, RadioButtons
 
 
 class Stonks:
-    def __init__(self, asset: str):
-        self.asset = asset
 
+    def __init__(self, asset: str):
+        self.ax = None
+        self.asset = asset
+        self.current_plot_type = 'candle'
+        self.indicators_width = 0.2
         self.df = self.download_historical_data(asset)
 
         if self.df.empty:
@@ -32,18 +37,58 @@ class Stonks:
         df = self.df.copy()
         df.index.name = 'Date'
 
-        indicators_width = 0.2
-
         add_plots = [
-            mpf.make_addplot(df['SMA'], color='green', label='SMA', width=indicators_width),
+            mpf.make_addplot(df['SMA'], color='green', label='SMA', width=self.indicators_width),
             mpf.make_addplot(df['Upper_Bollinger_Band'], color='red', linestyle='--', label='Upper Bollinger',
-                             width=indicators_width),
+                             width=self.indicators_width),
             mpf.make_addplot(df['Lower_Bollinger_Band'], color='blue', linestyle='--', label='Lower Bollinger',
-                             width=indicators_width)
+                             width=self.indicators_width)
         ]
 
-        mpf.plot(df, type='candle', style='charles', addplot=add_plots, title=f'{self.asset} Px Chart', ylabel='Price',
-                 volume=False, figsize=(14, 7), show_nontrading=True)
+        fig, axlist = mpf.plot(df, type=self.current_plot_type, style='charles', addplot=add_plots,
+                               title=f'{self.asset} Px Chart', ylabel='Price', volume=False, figsize=(14, 7),
+                               returnfig=True)
+        self.ax = axlist[0]
+        self.add_widget_box(fig)
+
+    def update_plot(self) -> None:
+        self.ax.clear()
+        self.ax.set_title(f'{self.asset} Px Chart')
+
+        add_plots_updated = [
+            mpf.make_addplot(self.df['SMA'], color='green', label='SMA', width=self.indicators_width, ax=self.ax),
+            mpf.make_addplot(self.df['Upper_Bollinger_Band'], color='red', linestyle='--', label='Upper Bollinger',
+                             width=self.indicators_width, ax=self.ax),
+            mpf.make_addplot(self.df['Lower_Bollinger_Band'], color='blue', linestyle='--', label='Lower Bollinger',
+                             width=self.indicators_width, ax=self.ax)
+        ]
+
+        mpf.plot(self.df, type=self.current_plot_type, style='charles', addplot=add_plots_updated, ax=self.ax,
+                 volume=False)
+
+        plt.draw()
+
+    def add_widget_box(self, fig) -> None:
+        ax_radio = plt.axes([0.05, 0.7, 0.1, 0.15])
+        radio = RadioButtons(ax_radio, ['candle', 'line', 'ohlc'])
+        radio.on_clicked(self.change_plot_type)
+
+        ax_textbox = plt.axes([0.05, 0.45, 0.1, 0.05])
+        textbox = TextBox(ax_textbox, 'Input', initial=self.asset)
+        textbox.on_submit(self.submit_text)
+
+        plt.show()
+
+    def change_plot_type(self, label: str) -> None:
+        self.current_plot_type = label
+        self.update_plot()
+
+    def submit_text(self, text: str) -> None:
+        new_asset = text.strip()
+        self.asset = new_asset
+        self.df = self.download_historical_data(new_asset)
+        self.df = self.calculate_indicators(self.df)
+        self.update_plot()
 
 
 def run_stocks_analysis(asset: str) -> None:
